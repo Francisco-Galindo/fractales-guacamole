4f26adb0ca5505a869b5e466712c326eeb2a0097
Francisco-Galindo <paqui10718@gmail.com>
2023-11-21 14:10:50 -0600

    Corrigiendo unidades en gráfica

--- plot.sh
+++ plot.sh
@@ -5,7 +5,7 @@ set term png
 set output "$2"
 set title "Rendimiento en función del número de hilos"
 set xlabel "Número de hilos"
-set ylabel "Tiempo de ejecución"
+set ylabel "Tiempo de ejecución (segundos)"
 set yrange[0:]
 set datafile separator "\t"
 plot for[col=2:$(awk -F' ' '{print NF}' $1 | head -n 1)] "$1" using 1:col title columnheader(col) with lines
